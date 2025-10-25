85608e52bd78abc184dc6e98e8fb7118b6b2d9bc
tzuping <tzupingkao@gmail.com>
2025-10-25 13:53:57 +0000

    debug for python

--- init.lua
+++ init.lua
@@ -698,6 +698,19 @@ require('lazy').setup({
             },
           },
         },
+
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true
+              }
+            }
+          }
+        },
+
       }
 
       -- Ensure the servers and tools above are installed
@@ -973,7 +986,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -94,7 +94,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        'debugpy',
       },
     }
 
@@ -135,6 +135,25 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    dap.adapters.python = {
+      type = 'executable',
+      command = 'python',
+      args = { '-m', 'debugpy.adapter' }
+    }
+
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = '当前文件',
+        program = '${file}',
+        pythonPath = function()
+          return '.venv/bin/python'
+        end,
+      }
+    }
+
 
     -- Install golang specific config
     require('dap-go').setup {
